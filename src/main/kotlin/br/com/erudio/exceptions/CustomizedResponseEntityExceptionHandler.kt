01b583e009e651be82eeb7bf240750847a1ede3a
Scavazin <scavadev@gmail.com>
2023-08-05 22:58:11 -0300

    Implementing Exceptions

---
+++ src/main/kotlin/br/com/erudio/exceptions/CustomizedResponseEntityExceptionHandler.kt
@@ -0,0 +1,38 @@
+package br.com.erudio.exceptions
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.context.request.WebRequest
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import java.util.*
+
+@RestController
+@ControllerAdvice // This annotation is used to handle exceptions globally
+class CustomizedResponseEntityExceptionHandler : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(Exception::class)
+    fun handleAllExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionResponse> {
+
+        val exceptionResponse = ExceptionResponse(
+            timestamp = Date(),
+            message = ex.message,
+            details = request.getDescription(false)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+    @ExceptionHandler(UnsupportedMathOperationException::class)
+    fun handleBadRequestExceptions(ex: Exception, request: WebRequest):
+            ResponseEntity<ExceptionResponse> {
+
+        val exceptionResponse = ExceptionResponse(
+            timestamp = Date(),
+            message = ex.message,
+            details = request.getDescription(true)
+        )
+        return ResponseEntity<ExceptionResponse>(exceptionResponse, HttpStatus.BAD_REQUEST)
+    }
+
+}
