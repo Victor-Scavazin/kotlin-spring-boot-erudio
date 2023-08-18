25f5628f863ea86d4f0e8b98b7f73ff7ef73bcb7
Scavazin <scavadev@gmail.com>
2023-08-17 21:05:03 -0300

    Adding Migrations

---
+++ src/main/resources/db/migration/V2__insert_persons.sql
@@ -0,0 +1,6 @@
+INSERT INTO person(id, first_name, last_name, address, gender) VALUES
+(10, 'John', 'Doe', 'City Jardim', 'Male' ),
+(11, 'Victor', 'Sca', 'OZ', 'Male' ),
+(12, 'Lauro', 'Gentleman', 'CENTRAL PARK', 'Male' ),
+(13, 'Heits', 'Crazy', 'Jundiai', 'Male' ),
+(14, 'Rick', 'Vasserna', 'Faria Lima', 'Male' );
