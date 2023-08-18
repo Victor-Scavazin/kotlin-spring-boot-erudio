be9025e78778d19b75b7c96eb2faea5e7eb1ff03
Scavazin <scavadev@gmail.com>
2023-08-17 21:19:09 -0300

    Exploring Json Properties

--- src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
+++ src/main/kotlin/br/com/erudio/data/vo/v1/PersonVO.kt
@@ -1,9 +1,22 @@
 package br.com.erudio.data.vo.v1
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+
+@JsonPropertyOrder(
+    "id",
+    "lastName",
+    "address",
+    "gender",
+    "firstName"
+)
 data class PersonVO(
     var id: Long = 0,
+    @field:JsonProperty("first_name")
     var firstName: String = "",
     var lastName: String = "",
     var address: String = "",
+    @field:JsonIgnore
     var gender: String = ""
 )
